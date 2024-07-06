9836af3b39d344b1f04a22d108fd4247f1f26699
Mathaporn <lhar.sangjan02@gmail.com>
2024-07-06 16:12:35 +0700

    first commit

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text ("Home")),
+        ),
+        body: Column(
+          children: [
+            Image.asset("images/pig.jpg"),
+            Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                children: [
+                  const Icon(Icons.call),
+                  Container(
+                    child: const Text("Call"),
+                  )
+                ],
+              ),
+              Column(
+                children: [
+                  const Icon(Icons.near_me),
+                  Container(
+                    child: const Text("Route"),
+                  )
+                ],
+              ),
+              Column(
+                children: [
+                  const Icon(Icons.share),
+                  Container(
+                    child: const Text("Share"),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
